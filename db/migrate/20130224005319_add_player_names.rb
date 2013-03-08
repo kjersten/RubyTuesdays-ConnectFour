2921e7a8373817f0d6dbb8662ce2a173f65d0d47
kjersten <kjersten.elias@gmail.com>
2013-03-07 17:18:17 -0800

    adjusting migrations because a 'change' migration doesn't support the change_table command so we must use up and down to be able to revert db changes

--- db/migrate/20130224005319_add_player_names.rb
+++ db/migrate/20130224005319_add_player_names.rb
@@ -1,9 +1,15 @@
 class AddPlayerNames < ActiveRecord::Migration
-  def change
+  def up
     change_table :games do |t|
       t.string   :name
       t.string   :red_player_name
       t.string   :blue_player_name
+    end
+  end
+
+  def down
+    change_table :games do |t|
+      t.remove   :name, :red_player_name, :blue_player_name
     end
   end
 
--- db/migrate/20130308002711_remove_game_name_and_initialize_player_names.rb
+++ db/migrate/20130308002711_remove_game_name_and_initialize_player_names.rb
@@ -1,9 +1,18 @@
 class RemoveGameNameAndInitializePlayerNames < ActiveRecord::Migration
-  def change
-    remove_column :games, :name
+  def up
+    change_table :games do |t|
+      t.remove   :name
+    end
     Game.reset_column_information
     Game.all.each do |game|
       game.update_attributes!(:red_player_name => 'red', :blue_player_name => 'blue')
     end
   end
+
+  def down
+    change_table :games do |t|
+      t.string   :name
+    end
+  end
+
 end
